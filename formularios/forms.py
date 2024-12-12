eedee41b55c4fb0e3401b439588dc1a7ea186606
Pistachopower <nelson.galicia.carrero.al@iespoligonosur.org>
2024-12-12 11:16:27 +0100

    form

--- formularios/forms.py
+++ formularios/forms.py
@@ -5,3 +5,35 @@ class UsuarioForm(forms.ModelForm):
     class Meta:
         model = Usuario
         fields = ('nombre', 'edad')
+        
+        
+class UsuarioForm(forms.ModelForm):
+    
+    class Meta:
+        model = Usuario
+        fields = ['nombre', 'edad']
+        help_texts = {
+            "nombre": ("200 caracteres como máximo"),
+        }
+        
+    
+    def clean(self):
+
+        super().clean()
+
+        nombre = self.cleaned_data.get('nombre')
+        edad = self.cleaned_data.get('edad')
+
+   
+        usuarioNombre = Usuario.objects.filter(nombre=nombre).first()
+        if(not usuarioNombre is None
+           ):
+             if(not self.instance is None and usuarioNombre.id == self.instance.id):
+                 pass
+             else:
+                self.add_error('nombre','Ya existe')
+
+            
+            
+        #Siempre devolvemos el conjunto de datos.
+        return self.cleaned_data
